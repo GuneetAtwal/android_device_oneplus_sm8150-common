a1ab948f4672f60b447910ebe720813083cb251a
GuneetAtwal <guneetatwal@gmail.com>
2025-03-28 14:31:07 -0700

    sm8150-common: Build TouchScreenGestures for Alert slider
    Signed-off-by: GuneetAtwal <guneetatwal@gmail.com>

--- common.mk
+++ common.mk
@@ -307,6 +307,7 @@ $(call inherit-product, hardware/oplus/oplus-fwk/oplus-fwk.mk)
 
 # Touch
 PRODUCT_PACKAGES += \
+    TouchGestures \
     vendor.lineage.touch@1.0-service.oplus
 
 $(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,INCLUDE_DIR,$(LOCAL_PATH)/touch/include)
