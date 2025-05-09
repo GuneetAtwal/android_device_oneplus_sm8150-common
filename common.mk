df1820cf0ffdf032371b92bf550e3c05a6543903
GuneetAtwal <guneetatwal@gmail.com>
2025-05-08 19:03:32 -0700

    sm8150-common: parts: Add per app maximum refresh rate
    Signed-off-by: GuneetAtwal <guneetatwal@gmail.com>

--- common.mk
+++ common.mk
@@ -113,6 +113,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    OPlusParts
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
